d0a1b5f5ee6bb1323e5b1dbf6b2f477b26f55d02
TadsGitDemo <that_tad_guy@hotmail.com>
2015-10-29 12:43:52 +1000

    no change made

--- BlueTeamAssignment/AddIncident.aspx.cs
+++ BlueTeamAssignment/AddIncident.aspx.cs
@@ -7,7 +7,7 @@ using System.Web.UI.WebControls;
 
 namespace BlueTeamAssignment
 {
-    public partial class CustomerIncidents : System.Web.UI.Page
+    public partial class AddIncident : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
